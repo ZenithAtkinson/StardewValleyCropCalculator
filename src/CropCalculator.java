31148898dcf7c400283947bca20c9e26761eaaf7
ZenithAtkinson <alexanderdavidatkinson@gmail.com>
2023-04-15 14:45:48 +0200

    finished debugging calculator

--- src/CropCalculator.java
+++ src/CropCalculator.java
@@ -2,13 +2,14 @@ import java.util.Scanner;
 public class CropCalculator {
 
     public static double calculateRevenue(givenCrop crop) { //calculates revenue based on user input.
-        int numberOfHarvests = 1;
+        int numberOfHarvests;
         // add bool conditions for different parts of the function. Make them parameters for the function itself ^
         int seasonLength = 28;  // in days
         int TotalSeasonLength = seasonLength * crop.getSeasonNumber(); // multiply season length by the number of seasons the crop grows (given it will be consecutively grown)
-        if (crop.getRepeatedHarvests()) {
-            int AdjustedSeasonLength = TotalSeasonLength - (crop.getGrowthTime() * crop.getSeasonNumber());
-            numberOfHarvests = AdjustedSeasonLength / (crop.getGrowthTime() * crop.getNumberofCrops());
+
+        if (crop.getRepeatedHarvests()) { //IF there are repeated harvests...
+            int AdjustedSeasonLength = TotalSeasonLength - crop.getGrowthTime();
+            numberOfHarvests = AdjustedSeasonLength / (crop.getRepeatedGrowthTime() * crop.getNumberofCrops()) + 1;
         } else {
             numberOfHarvests = TotalSeasonLength / (crop.getGrowthTime() * crop.getNumberofCrops()); // how many times the number of planted crops can be harvested in a season, given they are planted the day they are harvested.
         }
@@ -45,17 +46,9 @@ public class CropCalculator {
             System.out.println(message);
             String input = scanner.next().toLowerCase();
             switch (input) {
-                case "yes":
-                case "y":
-                    inputBoolean = true;
-                    break;
-                case "no":
-                case "n":
-                    inputBoolean = false;
-                    break;
-                default:
-                    System.out.println("Invalid input. Please try again.");
-                    break;
+                case "yes", "y" -> inputBoolean = true;
+                case "no", "n" -> inputBoolean = false;
+                default -> System.out.println("Invalid input. Please try again.");
             }
         }
         return inputBoolean;
--- src/Main.java
+++ src/Main.java
@@ -3,34 +3,34 @@
 import java.util.Scanner;
 /*
             TODO
-    -Finalize givenCrop.java
-    -Get math set up
-    -finish input lines and questions
-    -Create list with data on each crop type
-
+    -Inform user of how much a single harvest gave for the crop?
+    -create list of crops to choose from
+    -set up GUI
+        -create symbols / buttons
+        -import as .exe for computer / iphone?
  */
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        givenCrop userCrop = new givenCrop(10, 20, 100, 20, 1, false, 0);
-        boolean keepCalculating = true;
-        while(keepCalculating) {
-            //asking user for the data on a given crop.
-            userCrop.setCost(CropCalculator.getInputInt("How much does the crop seed cost in gp?", scanner));
-            userCrop.setSellPrice(CropCalculator.getInputInt("How much does the crop sell for in gp?", scanner));
-            userCrop.setNumberofCrops(CropCalculator.getInputInt("How many crops do you plan to plant?", scanner));
-            userCrop.setGrowthTime(CropCalculator.getInputInt("How long does it take for the plant to be fully-grown (harvestable)?", scanner));
+        try (Scanner scanner = new Scanner(System.in)) {
+            givenCrop userCrop = new givenCrop(10, 20, 100, 20, 1, false, 0);
+            boolean keepCalculating = true;
+            while (keepCalculating) {
+                //asking user for the data on a given crop.
+                userCrop.setCost(CropCalculator.getInputInt("How much does the crop seed cost in gp?", scanner));
+                userCrop.setSellPrice(CropCalculator.getInputInt("How much does the crop sell for in gp?", scanner));
+                userCrop.setNumberofCrops(CropCalculator.getInputInt("How many crops do you plan to plant?", scanner));
+                userCrop.setGrowthTime(CropCalculator.getInputInt("How long does it take for the plant to be fully-grown (harvestable)?", scanner));
 
-            userCrop.setRepeatedHarvest(CropCalculator.getInputBoolean("Does the crop have repeated harvests?", scanner));
-            if (userCrop.getRepeatedHarvests()) {
-                userCrop.setRepeatedGrowthTime(CropCalculator.getInputInt("How long does it take for the plant to regrow produce?", scanner));
+                userCrop.setRepeatedHarvest(CropCalculator.getInputBoolean("Does the crop have repeated harvests?", scanner));
+                if (userCrop.getRepeatedHarvests()) {
+                    userCrop.setRepeatedGrowthTime(CropCalculator.getInputInt("How long does it take for the plant to regrow produce?", scanner));
+                }
+                userCrop.setSeasonNumber(CropCalculator.getInputInt("How many seasons does the crop grow for?", scanner));
+
+                double profit = CropCalculator.calculateRevenue(userCrop);
+                System.out.println("The profit for the given crop is: " + profit);
+                keepCalculating = CropCalculator.getInputBoolean("Do you want to calculate another crop?", scanner);
             }
-            userCrop.setSeasonNumber(CropCalculator.getInputInt("How many seasons does the crop grow for?", scanner));
-
-            double profit = CropCalculator.calculateRevenue(userCrop);
-            System.out.println("The profit for the given crop is: " + profit);
-            keepCalculating = CropCalculator.getInputBoolean("Do you want to calculate another crop?", scanner);
         }
-        scanner.close();
     }
 }
